f9312e054c2844b3be954b5ada13de9cd60c254d
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-02-17 19:42:16 -0800

    imported QueuedSocket

--- include/Obbligato/Net_QueuedSocket.hpp
+++ include/Obbligato/Net_QueuedSocket.hpp
@@ -30,10 +30,138 @@ namespace Obbligato
 {
     namespace Net
     {
+        /// Base class for socket with with Incoming and Outgoing packet queues
+        template <typename QueueT, typename SocketT>
+        class QueuedSocket
+        {
+        public:
+            typedef QueueT queue_type;
+            typedef SocketT socket_type;
+            typedef typename QueueT::value_type packet_type;
+
+        private:
+            queue_type m_incoming_queue;
+            queue_type m_outgoing_queue;
+            socket_type *m_sock;
+
+        public:
+
+            /// Create a queued rawnet socket with incoming, outgoing, and raw socket
+            QueuedSocket(
+                         size_t queue_size,
+                         socket_type *sock
+                    ) :
+                m_incoming_queue(queue_size),
+                m_outgoing_queue(queue_size),
+                m_sock(sock)
+            {
+            }
+
+
+            virtual ~QueuedSocket()
+            {
+                delete m_sock;
+            }
+
+            /// Get a reference to the incoming queue
+            queue_type &incoming_queue() { return m_incoming_queue; }
+
+            /// Get a const reference to the incoming queue
+            queue_type const &incoming_queue() const { return m_incoming_queue; }
+
+            /// Get a reference to the outgoing queue
+            queue_type &outgoing_queue() { return m_outgoing_queue; }
+
+            /// Get a const reference to the outgoing queue
+            queue_type const &outgoing_queue() const { return m_outgoing_queue; }
+
+            /// Check if there is any data in the outgoing_queue to be sent over the socket
+            bool outgoing_not_empty() const { return !m_outgoing_queue.empty(); }
+
+            /// Check if there is any space in the outgoing_queue
+            bool outgoing_not_full() const { return !m_outgoing_queue.full(); }
+
+            /// Check if there is any data in the incoming_queue
+            bool incoming_not_empty() const { return !m_incoming_queue.empty(); }
+
+            /// Check if there is any space in the incoming_queue for received packets
+            bool incoming_not_full() const { return !m_incoming_queue.full(); }
+
+            /// Place a packet into the outgoing queue
+            bool send( packet_type const &outpkt )
+            {
+                bool r=false;
+                if( outgoing_not_empty())
+                {
+                    m_outgoing_queue.incoming() = outpkt;
+                    m_outgoing_queue.push();
+                    r=true;
+                }
+                return r;
+            }
+
+            /// Try receive a packet from the incoming queue
+            bool recv( packet_type &inpkt )
+            {
+                bool r=false;
+                if( incoming_not_empty())
+                {
+                    inpkt = m_incoming_queue.outgoing();
+                    m_incoming_queue.pop();
+                    r=true;
+                }
+                return r;
+            }
+
+            /// Send one packet from the outgoing_queue out the socket
+            bool send_one_from_queue_to_socket()
+            {
+                bool r=false;
+                // Is there a packet to send?
+                if( outgoing_not_empty() )
+                {
+                    // Yes, try send it
+                    if( m_sock->send(outgoing_queue().outgoing()) >= 0 )
+                    {
+                        // Success sending it, so pop the packet off the outgoing queue
+                        outgoing_queue().pop();
+
+                        // and show success
+                        r=true;
+                    }
+                }
+                return r;
+            }
+
+            /// Try receive one packet from the socket into the incoming_queue
+            bool receive_one_from_socket_into_queue()
+            {
+                bool r=false;
+                // Is there space to receive a packet?
+                if( incoming_not_full() )
+                {
+                    // Yes, try receive a packet into the queue
+                    if( m_sock->recv(incoming_queue().incoming()) >= 0 )
+                    {
+                        // Got one! push it into the incoming queue
+                        incoming_queue().push();
+
+                        // and show success
+                        r = true;
+                    }
+                }
+                return r;
+            }
+
+            /// Get a reference to the RawNetSocket
+            socket_type &sock() { return *m_sock; }
+
+            /// Get a const reference to the RawNetSocket
+            socket_type const &sock() const { return *m_sock; }
+
+        };
 
     }
-
-
 }
 
 #endif
